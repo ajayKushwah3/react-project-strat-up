62889d7ed8f581f19462fd8e065a6537c380b758
ajaykushwah3 <ajay1992kushwah@gmail.com>
2023-09-03 18:54:13 +0530

    add-code

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.scss';
+import { Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <div className='box-package'>
+        <h6>NPM Package</h6>
+        <p>npm i react-leaflet</p>
+        <p>npm i react-loaders</p>
+        <p>npm i firebase</p>
+        <p>npm i leaflet</p>
+
+        <h6>css Package</h6>
+        <p>npm i loaders.css</p>
+        <p>npm i sass</p>
+
+        <h6>fortawesome Package</h6>
+        <p>npm i @fortawesome/fontawesome-svg-core</p>
+        <p>npm i @fortawesome/free-brands-svg-icons</p>
+        <p>npm i @fortawesome/free-solid-svg-icons</p>
+        <p>npm i @fortawesome/react-fontawesome</p>
+
+        <h6>Router Package</h6>
+        <p>npm i react-router-dom</p>
+
+        <h6>Animate Package</h6>
+        <p>npm i gsap-trial</p>
+        <p>npm i animate.css</p>
+        <p>npm i framer-motion <a target='_blank'  href="https://www.framer.com/?utm_source=motion-readme">framer-motion</a> </p>
+        <p>npm i react-spring <a target='_blank'  href="https://www.react-spring.io/">react-spring</a> </p>
+        <p>npm i remotion <a  target='_blank' href="https://www.remotion.dev/">remotion</a> </p>
+        <p>npm i react-motion <a  target='_blank' href="https://github.com/chenglou/react-motion">react-motion</a> </p>
+        <p>npm i react-move <a  target='_blank' href="https://react-move-docs.netlify.app/">framer-motion</a> </p>
+        <p>npm i react-reveal <a  target='_blank' href=" https://www.react-reveal.com/">react-reveal</a> </p>
+        <p>npm i react-anim <a  target='_blank' href=" https://alain.xyz/libraries/react-anime">react-anim</a> </p>
+        <p>npm i styled-components <a  target='_blank' href="https://styled-components.com/">styled-components</a> </p>
+        <p>npm i react-vis <a  target='_blank' href="https://uber.github.io/react-vis">react-vis</a> </p>
+        
+        
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
